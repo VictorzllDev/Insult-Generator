116fbfbce643d40af858fe8ac72cdc39ea8c1e55
VictorzllDev <joaovictor12200capa@gmail.com>
2024-03-03 22:37:14 -0300

    resource to change the language of the sentence

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,10 +3,11 @@ import { getInsult } from '../../services/getInsult'
 
 export function Modal() {
   const [insult, setInsult] = useState('')
+  const [lang, setLang] = useState('EN')
 
   const generateInsult = async () => {
     try {
-      const insult = await getInsult()
+      const insult = await getInsult(lang)
       setInsult(insult)
     } catch {
       setInsult('')
@@ -16,7 +17,13 @@ export function Modal() {
   return (
     <div className="mx-auto my-7 flex max-w-screen-sm flex-col justify-between space-y-4 rounded bg-slate-300 p-4">
       <div className="flex justify-end">
-        <select name="lang" id="lang" className="rounded p-2">
+        <select
+          name="lang"
+          id="lang"
+          className="rounded p-2"
+          onChange={(e) => setLang(e.target.value)}
+          value={lang}
+        >
           <option>EN</option>
           <option>PT-BR</option>
         </select>
--- src/services/getInsult.ts
+++ src/services/getInsult.ts
@@ -7,19 +7,24 @@ interface InsultData {
 	insult: string
 }
 
-export async function getInsult() {
+export async function getInsult(lang: string) {
+	// Get the insults API.
 	const api = 'https://insult.mattbas.org/api/insult.json'
 	const data = axios.get<InsultData>(api)
 
+	// Check if there was an error in the request.
 	if ((await data).data.error) new Error('error em buscar insultos')
 
-	// Traduzir insulto para portugues
+	// If the requested language is the same as the language of the sentence, return the sentence.
+	if (lang.toLowerCase() == 'en') return (await data).data.insult
 
+	// Translates the English sentence into the requested language.
 	const insultPtBr = await axios.get(
 		`https://api.mymemory.translated.net/get?q=${
 			(await data).data.insult
-		}&langpair=en|pt-br`,
+		}&langpair=en|${lang.toLowerCase()}`,
 	)
 
+	// returns the sentence in the requested language.
 	return insultPtBr.data.responseData.translatedText
 }
